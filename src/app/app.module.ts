ac8f230f336d51a02963a2168714387fd9fbb912
Agustina Chiodini <agus.chio25790@gmail.com>
2023-05-13 01:36:37 -0300

    todos los compoenentes agregados y hechos, solo falta responsive telefono, y falta hys y agregue logo de argentina programa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    HysComponent,
+    ProyectosComponent,
+    FooterComponent,
+    LoginComponent,
+    RedesSocialesComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
